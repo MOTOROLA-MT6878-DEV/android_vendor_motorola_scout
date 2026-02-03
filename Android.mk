0b1f0095858587e09bd01656601423fac52667da
Notganesh <chimeports@gmail.com>
2026-02-03 12:26:00 +0000

    Scout: initial blobs from stock
    Signed-off-by: Notganesh <chimeports@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,29 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),scout)
+
+$(call add-radio-file-sha1-checked,radio/ccu.img,d1b79a13bfaa2021749fcbe635d3362cd0cef84c)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,f472ecd15081fe601010896ff518d743992a33d4)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,5632ba402549b6e17c3922aa8e84d9d6ac8b02df)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,91cc97f046953098dbb5126ccf98b20bd9d26ee0)
+$(call add-radio-file-sha1-checked,radio/dpm.img,40122cc0ac26ea49ff87ec4cf5720289b796cdcc)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,b1b37af61c00769016e6e04f08919ab3d00c4f60)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,a479146a119b775930eeeddb266efee8edfe2dee)
+$(call add-radio-file-sha1-checked,radio/gz.img,4defa9d812ece4d5912d44e008d06f1709750cd5)
+$(call add-radio-file-sha1-checked,radio/lk.img,c0af68fb1cff3d6410336b919f86c4643be55117)
+$(call add-radio-file-sha1-checked,radio/logo.img,7a5b033e885c19d388cfbf78db8428cddfef1cc4)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,f4033cec76fdd9a3625eca59ff2e667b7874b1f7)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,d2297c604de6ea23305a54b96c50586954b3cef0)
+$(call add-radio-file-sha1-checked,radio/modem.img,2c5b4a98b1c2b9db45f3bedd4654cf6a79072ba5)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,40333222954914ca1f8bad8bf6282a5bf0083a00)
+$(call add-radio-file-sha1-checked,radio/scp.img,d482f4d774d47727246f690fc62fc3eed60b531d)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,0ec963d74f6d6ab5b3542705afcafd746a572975)
+$(call add-radio-file-sha1-checked,radio/sspm.img,9136aade5d449fcbdbc67eaade617d25aa21ec78)
+$(call add-radio-file-sha1-checked,radio/tee.img,b9156304f9f48096197e8da570780efc5b163a9b)
+$(call add-radio-file-sha1-checked,radio/vcp.img,3bf6ab5ca3b7ee547d2db83003f6bdd48e6692c8)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,24 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    ccu \
+    connsys_bt \
+    connsys_gnss \
+    connsys_wifi \
+    dpm \
+    dtbo \
+    gpueb \
+    gz \
+    lk \
+    logo \
+    mcf_ota \
+    mcupm \
+    modem \
+    pi_img \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    vcp
